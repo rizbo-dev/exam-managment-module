f246aa0675ee226883b2be9a86542bb765c787d0
rizbo <boris.evetovic777@gmail.com>
2024-06-19 19:37:23 +0200

    Added mailer to email-service, added mailhog to stack

--- email-service/app/src/MessageHandler/StudentUpdateMessageHandler.php
+++ email-service/app/src/MessageHandler/StudentUpdateMessageHandler.php
@@ -3,13 +3,28 @@
 namespace App\MessageHandler;
 
 use App\Message\StudentUpdateMessage;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
 
 #[AsMessageHandler]
 class StudentUpdateMessageHandler
 {
+    public function __construct(
+        private readonly MailerInterface $mailer
+    )
+    {
+    }
+
     public function __invoke(StudentUpdateMessage $message)
     {
-        dump($message);
+        $email = (new Email())
+            ->from('boris@boris.com')
+            ->to('boris@boris.com')
+            ->subject('Test')
+            ->text('sending emails')
+            ->html('<h1>Hello</h1>');
+
+        $this->mailer->send($email);
     }
 }

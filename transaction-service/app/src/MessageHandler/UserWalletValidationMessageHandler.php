e0b1cdc04fa4a360da1b5fd22f9579958bedea57
rizbo <boris.evetovic777@gmail.com>
2024-08-26 19:34:27 +0200

    Finished UserWalletValidationMessageHandler.php

--- transaction-service/app/src/MessageHandler/UserWalletValidationMessageHandler.php
+++ transaction-service/app/src/MessageHandler/UserWalletValidationMessageHandler.php
@@ -2,30 +2,69 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Wallet;
 use App\Message\ResponseSagaItemMessage;
 use App\Message\UserWalletValidationMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsMessageHandler]
 readonly class UserWalletValidationMessageHandler
 {
     public function __construct(
         private MessageBusInterface $messageBus,
-
+        private HttpClientInterface $examClient,
+        private EntityManagerInterface $entityManager
     )
     {
     }
 
     public function __invoke(UserWalletValidationMessage $message)
     {
-        /**
-         * TODO:
-         * 1. Fetch exam cost
-         * 2. Compare it to wallet
-         * 3. If there is not enough credit in wallet trigger isValid false
-         * 4. If there is enough credit in wallet trigger isValid true
-         */
+        $exam = $this->examClient->request(Request::METHOD_GET, '/api/exams/' . $message->getExamId())->toArray();
+
+        $examCost = $exam['cost'];
+
+        /** @var Wallet|null $wallet */
+        $wallet = $this->entityManager->getRepository(Wallet::class)->findOneBy([
+            'studentId' => $message->getStudentId()
+        ]);
+
+        if (!$wallet) {
+            $message = new ResponseSagaItemMessage(
+                status: 'finished',
+                sagaType: ResponseSagaItemMessage::USER_WALLET_VALIDATION_SAGA_ITEM_TYPE,
+                examRegistrationId: $message->getExamRegistrationId(),
+                payload: [
+                    'isValid' => false,
+                    'message' => 'Student wallet not found!'
+                ]
+            );
+
+            $this->messageBus->dispatch($message);
+
+            return;
+        }
+
+        if (($wallet->getAmount() - $examCost) < 0) {
+            $message = new ResponseSagaItemMessage(
+                status: 'finished',
+                sagaType: ResponseSagaItemMessage::USER_WALLET_VALIDATION_SAGA_ITEM_TYPE,
+                examRegistrationId: $message->getExamRegistrationId(),
+                payload: [
+                    'isValid' => false,
+                    'message' => 'Not enough funds on wallet'
+                ]
+            );
+
+            $this->messageBus->dispatch($message);
+
+            return;
+        }
+
 
         $message = new ResponseSagaItemMessage(
             status: 'finished',
@@ -33,7 +72,7 @@ readonly class UserWalletValidationMessageHandler
             examRegistrationId: $message->getExamRegistrationId(),
             payload: [
                 'isValid' => true,
-                'examCreditValue' => 1000
+                'examCreditValue' => $examCost
             ]
         );
 

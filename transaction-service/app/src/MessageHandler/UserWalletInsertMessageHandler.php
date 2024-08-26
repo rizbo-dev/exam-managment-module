96e1513d0c740dae8501b40e8457650e7289b807
rizbo <boris.evetovic777@gmail.com>
2024-08-26 19:44:48 +0200

    Finished UserWalletInsertMessageHandler.php

--- transaction-service/app/src/MessageHandler/UserWalletInsertMessageHandler.php
+++ transaction-service/app/src/MessageHandler/UserWalletInsertMessageHandler.php
@@ -2,8 +2,11 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Transaction;
+use App\Entity\Wallet;
 use App\Message\ResponseSagaItemMessage;
 use App\Message\UserWalletInsertMessage;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -11,24 +14,37 @@ use Symfony\Component\Messenger\MessageBusInterface;
 readonly class UserWalletInsertMessageHandler
 {
     public function __construct(
-        private MessageBusInterface $messageBus
+        private MessageBusInterface $messageBus,
+        private EntityManagerInterface $entityManager
     )
     {
     }
 
     public function __invoke(UserWalletInsertMessage $message): void
     {
-        /*
-         * TODO:
-         * Descrease amount on user wallet
-         */
+        /** @var Wallet $userWallet */
+        $userWallet = $this->entityManager->getRepository(Wallet::class)->findOneBy([
+            'studentId' => $message->getStudentId()
+        ]);
+
+        $transaction = new Transaction();
+        $transaction
+            ->setStatus('processed')
+            ->setAmount($message->getAmount())
+            ->setType('outcome');
+
+        $this->entityManager->persist($transaction);
+
+        $userWallet->addTransaction($transaction);
+
+        $this->entityManager->flush();
 
         $message = new ResponseSagaItemMessage(
             status: 'finished',
             sagaType: ResponseSagaItemMessage::USER_WALLET_INSERT_SAGA_ITEM_TYPE,
             examRegistrationId: $message->getExamRegistrationId(),
             payload: [
-                'transactionId' => 1
+                'transactionId' => $transaction->getId()
             ]
         );
 

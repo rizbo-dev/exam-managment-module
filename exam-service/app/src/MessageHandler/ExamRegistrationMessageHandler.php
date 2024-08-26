aeea80409b821bb9fb1c93018067d14bc127fa0b
rizbo <boris.evetovic777@gmail.com>
2024-08-26 19:58:35 +0200

    Finished ExamRegistrationMessageHandler.php

--- exam-service/app/src/MessageHandler/ExamRegistrationMessageHandler.php
+++ exam-service/app/src/MessageHandler/ExamRegistrationMessageHandler.php
@@ -2,28 +2,41 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Exam;
+use App\Entity\ExamStudent;
 use App\Entity\SagaItem;
 use App\Message\ExamRegistrationMessage;
 use App\Message\ResponseSagaItemMessage;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
-class ExamRegistrationMessageHandler
+readonly class ExamRegistrationMessageHandler
 {
     public function __construct(
-        private readonly MessageBusInterface $messageBus
+        private MessageBusInterface    $messageBus,
+        private EntityManagerInterface $entityManager
     )
     {
     }
 
-    public function __invoke(ExamRegistrationMessage $message)
+    public function __invoke(ExamRegistrationMessage $message): void
     {
+        $examStudent = new ExamStudent();
+        $exam = $this->entityManager->getRepository(Exam::class)->find($message->getExamId());
+
+        $examStudent->setExam($exam)
+                    ->setStudentId($message->getStudentId());
+
+        $this->entityManager->persist($examStudent);
+        $this->entityManager->flush();
+
         $responseSagaItem = new ResponseSagaItemMessage();
         $responseSagaItem
             ->setExamRegistrationId($message->getExamRegistrationId())
             ->setPayload([
-                'examStudentId' => 12
+                'examStudentId' => $examStudent->getId()
             ])
             ->setStatus('finished')
             ->setSagaType('examRegistrationSagaItem');
